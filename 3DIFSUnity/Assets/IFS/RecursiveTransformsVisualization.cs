f272f7205f0475997df4d9207b83bcf37c346724
Forrest Trepte <forresttreptejunk@hotmail.com>
2019-05-19 13:16:57 -0700

    interpolated float LevelOfRecursion

--- 3DIFSUnity/Assets/IFS/RecursiveTransformsVisualization.cs
+++ 3DIFSUnity/Assets/IFS/RecursiveTransformsVisualization.cs
@@ -13,6 +13,7 @@ namespace IFS
         private CubeMeshSource cube = new CubeMeshSource();
         private ProceduralMesh pm = new ProceduralMesh();
         private bool needsUpdate = true;
+        private float previousLevelOfRecursion;
 
         private void Start()
         {
@@ -23,9 +24,15 @@ namespace IFS
 
         private void Update()
         {
+            if (previousLevelOfRecursion != LevelOfRecursion)
+            {
+                needsUpdate = true;
+            }
+
             if (needsUpdate)
             {
                 Visualize(definition?.Definition);
+                previousLevelOfRecursion = LevelOfRecursion;
                 needsUpdate = false;
             }
         }
@@ -57,24 +64,46 @@ namespace IFS
             cube.Init();
             Matrix4x4[] transforms = definition.Definition.GetMatrices();
             pm.Reset();
-            VisualizeToMeshRecursive(Matrix4x4.identity, 0, transforms);
+            VisualizeToMeshRecursive(Matrix4x4.identity, 0, transforms, Matrix4x4.identity);
             pm.ToMesh(mesh);
         }
 
-        private void VisualizeToMeshRecursive(Matrix4x4 transform, int level, Matrix4x4[] transforms)
+        private void VisualizeToMeshRecursive(Matrix4x4 transform, int level, Matrix4x4[] transforms, Matrix4x4 parentTransform)
         {
             if (level >= LevelOfRecursionInt - 1)
             {
                 // Leaf at final level of recursion. Output with this transform.
-                pm.AddTransformed(cube, transform);
+                if (LevelOfRecursion == LevelOfRecursionInt)
+                    pm.AddTransformed(cube, transform);
+                else
+                    pm.AddTransformed(cube, MatrixTrsLerp(parentTransform, transform, 1.0f + LevelOfRecursion - LevelOfRecursionInt));
                 return;
             }
 
             for (int i = 0; i < transforms.Length; ++i)
             {
                 Matrix4x4 childTransform = transforms[i] * transform;
-                VisualizeToMeshRecursive(childTransform, level + 1, transforms);
+                VisualizeToMeshRecursive(childTransform, level + 1, transforms, transform);
             }
+        }
+
+        private Matrix4x4 MatrixTrsLerp(Matrix4x4 a, Matrix4x4 b, float t)
+        {
+            // $TODO: Test that this works correctly with rotations and ordered combinations of all three transforms. Unit test?
+            GetMatrixTrs(a, out Vector3 aT, out Quaternion aR, out Vector3 aS);
+            GetMatrixTrs(b, out Vector3 bT, out Quaternion bR, out Vector3 bS);
+            Matrix4x4 result = Matrix4x4.TRS(
+                    Vector3.Lerp(aT, bT, t),
+                    Quaternion.Slerp(aR, bR, t),
+                    Vector3.Lerp(aS, bS, t));
+            return result;
+        }
+
+        private void GetMatrixTrs(Matrix4x4 matrix, out Vector3 translation, out Quaternion rotation, out Vector3 scale)
+        {
+            translation = matrix.GetColumn(3);
+            rotation = Quaternion.LookRotation(matrix.GetColumn(2), matrix.GetColumn(1));
+            scale = new Vector3(matrix.GetColumn(0).magnitude, matrix.GetColumn(1).magnitude, matrix.GetColumn(2).magnitude);
         }
     }
 }
